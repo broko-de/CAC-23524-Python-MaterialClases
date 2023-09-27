02f2d1f7d1ab8f56902596e9a7a078b4142ed644
Fede brokode <josefliquin@gmail.com>
2023-09-26 23:59:21 -0300

    Se agrega ejemplos de estructuras de control switch-case, if ternario y repetitivas

--- javascript/js/clase14.js
+++ javascript/js/clase14.js
@@ -22,3 +22,64 @@ if (age == 13){
 }else{
     console.warn('No tienes la edad suficiente, no puedes verla');
 }
+
+
+//ESTRUCTURA CONDICION SWITCH-CASE
+function parseRatingToStars(rating){
+    switch (parseInt(rating)) {
+        case 5:
+            console.log('⭐⭐⭐⭐⭐');
+            break;
+        case 4:
+            console.log('⭐⭐⭐⭐');
+            break;
+        case 3:
+            console.log('⭐⭐⭐');
+            break;
+        case 2:
+            console.log('⭐⭐');
+            break;
+        case 1:
+            console.log('⭐');
+            break;
+        default:
+            console.log('---');
+            break;
+    }
+}
+
+// parseRatingToStars(4.9);
+// OPERADOR CONDICIONAL TERNARIO
+function checkWatchMovie2(age,hasTicket){
+    //asignacion de un valor de acuerdo a la comprobación de una condición
+    let message = (age>=13 && hasTicket)?
+                    'Puedes ver la pelicula':
+                    'No cumples con los requisitos 😣';
+    console.log(message);
+}
+
+// checkWatchMovie2(5,true);
+// checkWatchMovie2(15,true);
+
+//ESTRUCTURA DE CONTROL REPETITIVA - WHILE
+
+function sellTicket(numberTicket){
+    let ticket = 1;
+    while(ticket <= numberTicket){
+        console.log('Ticket vendido nro:', ticket);
+        ticket = ticket+1;
+    }
+    console.log('Se vendieron todos los tickets');
+}
+
+// sellTicket(3);
+
+// ESTRUCTURA REPETITIVA - FOR
+function sellTicketFor(numberTicket){
+    for(let i=1;i<=numberTicket;i++){
+        console.log('Ticket vendido nro:', i);
+    }
+    console.log('Se vendieron todos los tickets con FOR');
+}
+
+sellTicketFor(4);
